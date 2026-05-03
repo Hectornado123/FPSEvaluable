9f52c2670ae9f7106f92ba4f528406a6fa0dbdba
Dakasta <diegpsanrracho@gmail.com>
2026-05-03 19:23:21 +0200

    fdghj

--- FPSProject/Assets/FPS_RPMI/scripts/player/GunSystem.cs
+++ FPSProject/Assets/FPS_RPMI/scripts/player/GunSystem.cs
@@ -4,7 +4,6 @@ using UnityEngine.InputSystem;
 
 public class GunSystem : MonoBehaviour
 {
-
     #region General Variables
 
     [Header("General References")]
@@ -12,7 +11,6 @@ public class GunSystem : MonoBehaviour
     [SerializeField] Transform ShootPoint;
     [SerializeField] LayerMask impactLayer;
     RaycastHit hit;
-
 
     [Header("Weapon Parameters")]
     [SerializeField] int damage = 10;
@@ -22,7 +20,6 @@ public class GunSystem : MonoBehaviour
     [SerializeField] float reloadTime = 1.5f;
     [SerializeField] bool allowButtonHold = false;
 
-
     [Header("Bullet Management")]
     [SerializeField] int amoSize = 30;
     [SerializeField] int bulletsPerTap = 1;
@@ -31,47 +28,61 @@ public class GunSystem : MonoBehaviour
     [Header("FeedBack references")]
     [SerializeField] GameObject impactEffect;
 
+    [Header(" Muzzle Flash")]
+    [SerializeField] GameObject muzzleFlashPrefab;
+
+    [Header(" Sonido disparo")]
+    [SerializeField] AudioSource shootAudioSource;
+    [SerializeField] AudioClip shootSound;
+
+    [Header(" Recoil Cámara")]
+    [SerializeField] float recoilX = 2f;
+    [SerializeField] float recoilY = 1f;
+    [SerializeField] float recoilReturnSpeed = 5f;
+    [SerializeField] float recoilSnappiness = 10f;
+
+    private Vector3 currentRotation;
+    private Vector3 targetRotation;
 
     [Header("Dev - Gun State Bools")]
     [SerializeField] bool shooting;
     [SerializeField] bool canShoot;
     [SerializeField] bool reloading;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     #endregion
+
     private void Awake()
     {
         bulletsLeft = amoSize;
         canShoot = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // DISPARO
         if (canShoot && shooting && !reloading && bulletsLeft > 0)
         {
-
             StartCoroutine(ShootRoutine());
-
-
         }
-    }
 
+        //  RECOIL UPDATE (SIEMPRE ACTIVO)
+        targetRotation = Vector3.Lerp(targetRotation, Vector3.zero, recoilReturnSpeed * Time.deltaTime);
+        currentRotation = Vector3.Slerp(currentRotation, targetRotation, recoilSnappiness * Time.deltaTime);
+        fpsCam.transform.localRotation = Quaternion.Euler(currentRotation);
+    }
 
     IEnumerator ShootRoutine()
     {
-
-
-
         canShoot = false;
+
         if (!allowButtonHold) shooting = false;
-        for (int i =0; i < bulletsPerTap; i++)
+
+        for (int i = 0; i < bulletsPerTap; i++)
         {
             if (bulletsLeft <= 0) break;
 
             Shoot();
             bulletsLeft--;
-
         }
 
         yield return new WaitForSeconds(shootingColldown);
@@ -80,6 +91,20 @@ public class GunSystem : MonoBehaviour
 
     void Shoot()
     {
+        //  SONIDO
+        if (shootAudioSource && shootSound)
+            shootAudioSource.PlayOneShot(shootSound);
+
+        //  MUZZLE FLASH
+        if (muzzleFlashPrefab != null && ShootPoint != null)
+        {
+            GameObject flash = Instantiate(muzzleFlashPrefab, ShootPoint.position, ShootPoint.rotation);
+            Destroy(flash, 0.15f);
+        }
+
+        //  RECOIL
+        ApplyRecoil();
+
         Vector3 direction = fpsCam.transform.forward;
 
         direction.x += Random.Range(-spread, spread);
@@ -89,23 +114,22 @@ public class GunSystem : MonoBehaviour
         {
             Debug.Log(hit.collider.name);
 
-            //if (hit.collider.CompareTag("enemy"))
-           // {
-              //  HealthEnemy healthEnemy = hit.collider.GetComponent<HealthEnemy>();
-               // healthEnemy.TakeDamage(damage);
-            //}
-
-            if (hit.collider.CompareTag("enemy"))
+            //  IMPACTO
+            if (impactEffect != null)
             {
-
-                Robot robot = hit.collider.GetComponent<Robot>();
-                Destroy(hit.collider.gameObject);
-
+                GameObject impact = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impact, 0.5f);
             }
 
+            //  ENEMIGO
+            if (hit.collider.CompareTag("enemy"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+
+            //  ORBE
             if (hit.collider.CompareTag("Esfera"))
             {
-                // Buscar el script destruible
                 Destruible1 destruible = hit.collider.GetComponent<Destruible1>();
 
                 if (destruible != null)
@@ -118,11 +142,17 @@ public class GunSystem : MonoBehaviour
         }
     }
 
-
+    void ApplyRecoil()
+    {
+        targetRotation += new Vector3(
+            -recoilX,
+            Random.Range(-recoilY, recoilY),
+            0
+        );
+    }
 
     IEnumerator ReloadRoutine()
     {
-
         reloading = true;
 
         yield return new WaitForSeconds(reloadTime);
@@ -130,45 +160,31 @@ public class GunSystem : MonoBehaviour
         reloading = false;
     }
 
-
     void Reload()
     {
-
         if (bulletsLeft < amoSize && !reloading)
         {
-
-
             StartCoroutine(ReloadRoutine());
         }
     }
+
     #region Input Methods
 
     public void OnShoot(InputAction.CallbackContext context)
     {
         if (allowButtonHold)
         {
-
             shooting = context.ReadValueAsButton();
-
-
         }
         else
         {
-
             if (context.performed) shooting = true;
-
-
         }
-
-
     }
-
 
     public void OnReload(InputAction.CallbackContext context)
     {
-
         if (context.performed) Reload();
-
     }
 
     #endregion
